523ca483ff0857df89693d52f247bf85e8db5621
Qorq-byte <3145971253@qq.com>
2026-09-19 19:59:44 +0800

    feat: implement local document ingestion

--- src/ragdb/application/__init__.py
+++ src/ragdb/application/__init__.py
@@ -1,5 +1,7 @@
 """Application services coordinating domain ports."""
 
 from ragdb.application.collections import CollectionService
+from ragdb.application.ingestion import LocalIngestionService
+from ragdb.application.sources import SourceService
 
-__all__ = ["CollectionService"]
+__all__ = ["CollectionService", "LocalIngestionService", "SourceService"]
